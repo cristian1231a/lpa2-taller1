38d2078b5d2a537ab579674b50937ab2398ddaa1
Cristian Yepes <camilo24021@gmail.com>
2025-10-23 13:56:09 -0500

    arreglo de test_mueble

--- tests/unit/models/test_mueble.py
+++ tests/unit/models/test_mueble.py
@@ -7,7 +7,7 @@ class TestMueble:
     def test_es_clase_abstracta(self):
         # Verificar que Mueble es abstracta
         with pytest.raises(TypeError):
-                     = Mueble("Mesa", "Madera", 100.0)
+                    mueble = Mueble("Mesa", "Madera", 100.0)
 
     def test_tiene_metodos_abstractos(self):
         # Verificar que tiene métodos abstractos
